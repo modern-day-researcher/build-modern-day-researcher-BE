deffa20e7c2ebe5585b4dd10533f03ad6eb31128
cori-peck <cori.peck@ymail.com>
2019-04-15 13:28:56 -0500

    updated knex file seeded user table

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './data/dev.sqlite3'
-    },
-    useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: { filename: './data/dev.sqlite3' },
+    useNullAsDefault: true, // used to avoid warning on console
     migrations: {
       directory: './data/migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
 };
