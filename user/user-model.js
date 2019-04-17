71c3ff897063356f1614fe8e4d7cae2f65d65631
cori-peck <cori.peck@ymail.com>
2019-04-16 20:05:31 -0500

    updates to readme

--- user/user-model.js
+++ user/user-model.js
@@ -5,17 +5,17 @@ module.exports = {
 };
 
 async function getFullUserData(userId) {
-  // const user = await db("users")
-  //     .select("users.first_name", "users.last_name")
-  //     .where("id", userId)
-  //     .first();
+   //const user = await db("users")
+       //.select("users.first_name", "users.last_name")
+       //.where("id", userId)
+       //.first();
 
-  // const articles = await db("users")
-  //     .join("articles", "users.id", "articles.user_id")
-  //     .select("articles.category", "articles.title", "articles.url", "articles.description")
-  //     .where("users.id", userId)
+   //const articles = await db("users")
+       //.join("articles", "users.id", "articles.user_id")
+       //.select("articles.category", "articles.title", "articles.url", "articles.description")
+       //.where("users.id", userId)
 
   return db("articles").where({ user_id: userId });
 
-  //   return { ...user, articels };
+     //return { ...user, articels };
 }
