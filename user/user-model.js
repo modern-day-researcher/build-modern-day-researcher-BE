a99a4eb34a22c49ac412b8e6cf92ffc582ec76d0
cori-peck <cori.peck@ymail.com>
2019-04-17 08:53:43 -0500

    added get list of users & post/del articles

--- user/user-model.js
+++ user/user-model.js
@@ -1,8 +1,19 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  getFullUserData
+  getUsers,
+  getFullUserData,
+  updateReadStatus,
+  addArticle,
+  getArticleById,
+  removeArticle
 };
+
+
+async function getUsers() {
+  return db('users').select('id', 'username', 'password', 'first_name', 'last_name')
+}
+
 
 async function getFullUserData(userId) {
    //const user = await db("users")
@@ -18,4 +29,39 @@ async function getFullUserData(userId) {
   return db("articles").where({ user_id: userId });
 
      //return { ...user, articels };
+}
+
+
+function updateReadStatus(userId, article) {
+  return db('articles')
+    .where('user_id', userId)
+    .update(article, "id")
+}
+
+
+async function addArticle(article) {
+  const [ id ] = await db('projects')
+    .insert(article, "id");
+
+    const newArticle = await getArticleById(id);
+
+    return newArticle;
+}
+
+
+function getArticleById(id) {
+  const article = db("articles")
+    .select("id", "title")
+    .where("id", id)
+    .first()
+
+    return article
+}
+
+
+function removeArticle(id) {
+  console.log('removeArticle', id);
+  return db('articles')
+  .where('id', id)
+  .del();
 }
--- user/user-routes.js
+++ user/user-routes.js
@@ -2,14 +2,90 @@ const router = require('express').Router();
 const User = require('./user-model');
 
 
+
+//get list of users
+
+router.get('/users', (req, res) => {
+    User
+    .getUsers()
+    .then(allUsers => {
+        res.status(200).json(allUsers)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "Could not retrieve list of users" })
+    })
+})
+
+
+
+
+//get list of articles saved to a single user
+
 router.get('/:id', (req, res) => {
     User
     .getFullUserData(req.params.id)
     .then(userData => {
         res.status(200).json(userData)
     })
-    .catch(err => {
-        res.status(500).json({ message: "Could not retrieve user data." })
+    .catch(error => {
+        res.status(500).json({ error: "Could not retrieve user data." })
+    })
+})
+
+
+//router.patch('/:id/read', async (req, res) => {
+    //const { is_read } = req.body;
+    //const { id } = req.params;
+//})
+
+
+//add an article to a users list
+
+router.post(':id/articles', (req, res) => {
+    const { title, url } = req.body;
+    const { id } = req.params;
+
+    if (!title && !url) {
+        res.status(400).json({ message: "Please enter an article title and url." })
+    } else {
+        req.body.user_id = id;
+        User
+        .addArticle(req.body)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Could not add article" })
+        })
+    }
+})
+
+
+//delete a users article from list
+
+router.delete('/:id/articles/:artId', (req, res) => {
+    let userId = req.params.id;
+    let artId = req.params.artId;
+
+    User
+    .getArticleById(artId)
+    .then(article => {
+        if (userId === `${article.user_id}`) {
+            User
+            .removeArticle(artId)
+            .then(removed => {
+                if (removed > 0) {
+                    releaseEvents.status(200).json({ message: 'Article removed from user' })
+                } else {
+                    res.status(404).json({ message: 'The article selected does not exist' })
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: "Error removing article" })
+            })
+        } else {
+            res.status(403).json({ message: "Selected article is not shared with this user" })
+        }
     })
 })
 
