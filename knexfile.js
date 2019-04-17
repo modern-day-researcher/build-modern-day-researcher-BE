b84943e1a35de7c5fbd85a3f30c4692a56f864fd
Brandon L. Borrero <borrero509@outlook.com>
2019-04-16 17:51:37 -0700

    working on heroku

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: "sqlite3",
-    connection: { filename: "./data/dev.sqlite3" },
+    connection: { filename: "./data/dev.db3" },
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations",
